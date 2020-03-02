ffac12f03e9bf51ca43a8a24cbede65cb7aa1f05
Rahat Chowdhury <rahat@rahatcodes.com>
2020-03-01 23:28:32 -0500

    initial config (#1)

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>CoEpi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default App;
