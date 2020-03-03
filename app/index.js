241e12fe9db6ee150cfb7743e5288378d161bd37
Emily Mills <emily@emlove.me>
2020-03-02 22:30:51 -0500

    Move react native app to its own folder

---
+++ app/index.js
@@ -0,0 +1,3 @@
+import App from './app.js';
+
+export default App;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './app';
+import {name as appName} from './config.json';
 
 AppRegistry.registerComponent(appName, () => App);
